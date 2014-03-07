84dd4983f8c79f06ce792dee3531221bf8b876cd
Lin Chen <qkthomas@gmail.com>
2014-03-06 21:52:44 -0500

    Initial commit. The XmlSerializer does work.

---
+++ Corpus/Corpora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlSerializer_Test
+{
+    public class Corpora
+    {
+        public List<List<int>> mList = new List<List<int>>();
+
+        public Corpora()
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                List<int> lst_tmp = new List<int>();
+                for (int j = 0; j < 26; j++)
+                {
+                    lst_tmp.Add(1);
+                }
+                this.mList.Add(lst_tmp);
+            }
+        }
+    }
+}
---
+++ Corpus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace XmlSerializer_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Corpora cops = new Corpora();
+            XmlSerializer ser = new XmlSerializer(typeof(Corpora));
+            string filename = "data.xml";
+            TextWriter writer = new StreamWriter(filename);
+            ser.Serialize(writer, cops);
+        }
+    }
+}
