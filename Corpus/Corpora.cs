b2754977a9b6a1d5f4ba01a498645d44804e1608
Lin Chen <qkthomas@gmail.com>
2014-03-08 16:53:33 -0500

    added mNumberOfOccuredBigrams field and IrrigateBigram() method

--- Corpus/Corpora.cs
+++ Corpus/Corpora.cs
@@ -9,6 +9,7 @@ namespace Corpus
 {
     public class Corpora
     {
+        private int mNumberOfOccuredBigrams = 0;
         private List<List<CorpEle>> mList = new List<List<CorpEle>>();
 
         public Corpora()
@@ -33,12 +34,28 @@ namespace Corpus
             }
         }
 
+        public void IrrigateBigram(char first_character, char second_character)
+        {
+            int base_index = (int)'a';
+            int x_index = (int)first_character - base_index;
+            int y_index = (int)second_character - base_index;
+            this.mList[x_index][y_index]++;
+            this.mNumberOfOccuredBigrams++;
+        }
+
         [XmlArray("Bigram")]
         [XmlArrayItem("Bigram-row", typeof(List<CorpEle>))]
         public List<List<CorpEle>> Bigram
         {
             get { return this.mList; }
             set { }
+        }
+
+        [XmlElement("Total-number-of-bigrams")]
+        public int NumberOfOccuredBigrams
+        {
+            get { return this.mNumberOfOccuredBigrams; }
+            set { this.mNumberOfOccuredBigrams = value; }
         }
     }
 }
